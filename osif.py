e05f123c113be67e025894e9be9897211ca1950c
Nicoleus1 <55134353+nicoleus@users.noreply.github.com>
2019-10-27 17:00:39 +0700

    osif.py

--- osif.py
+++ osif.py
@@ -486,7 +486,7 @@ def menu_reaction_ask():
   try:
 	global type
 
-	cek = raw_input(R + 'D3b2y' + W + '/' + R + 'Bot' + W + '/' + R + 'Reaction' + W + ' >> ')
+	cek = raw_input(R + 'nico' + W + '/' + R + 'Bot' + W + '/' + R + 'Reaction' + W + ' >> ')
 
 	if cek in ['1','01']:
 		type = 'LIKE'
@@ -568,7 +568,7 @@ def bot():
   try:
 	global type , message , id , WT , token
 
-	cek = raw_input(R + 'D3b2y' + W +'/' + R +'Bot ' + W + '>> ')
+	cek = raw_input(R + 'nico' + W +'/' + R +'Bot ' + W + '>> ')
 
 	if cek in ['1','01']:
 		menu_reaction()
